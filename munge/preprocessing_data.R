ca3c8e7fd820f8aecf578b5a7d16ed56dcc49733
Bryan <ling551@we22960.emsl.pnl.gov>
2013-09-24 16:05:27 -0700

    added basic ggplot2 plottingdiagnostics

--- munge/preprocessing_data.R
+++ munge/preprocessing_data.R
@@ -9,11 +9,11 @@ ws=readWorksheetFromFile('20130917_MDAMB231_HMEC_Anova_Data.xlsx',sheet=1,header
 
 response.var=ws$Fold.Change
 summary(response.var)
-
+   
 # determine data coding 
 rm(bworkbook)
 bworkbook=loadWorkbook('../diagnostics/levels.xls',create=TRUE)
-createSheet(bworkbook,name='levels')
+createSheet(bworkbook,name='levplotels')
 description=paste((date()),'diagnostics: LEVELS of column data')
 writeWorksheet(description,object=bworkbook, sheet='levels', startRow=1,header=F)
 writeWorksheet(t(colnames(ws)),object=bworkbook, sheet='levels', startRow=3, startCol=2)
@@ -51,3 +51,24 @@ ws.factored.v2=ws.factored[-which(ws.factored$Fold.Change==(max(ws.factored$Fold
 jpeg(file='../diagnostics/boxplot_factored_data.v2.jpeg')
 boxplot(ws.factored.v2,las=2)
 dev.off()
+
+#print out summary
+write.csv(data.frame(summary(ws.factored.v2)),'../diagnostics/summary_ws_factored_v2.csv')
+
+# look at basic plots 
+pairs(~Fold.Change+TGFB+EGF+Serum,data=ws,log='xy')
+
+jpeg()
+dev.off()
+
+require(ggplot2)
+attach(ws)
+plot1 = ggplot(data=ws,aes(x=Fold.Change,y=TGFB))+geom_jitter(aes(color=Cell.Line, size=EGF, ))+ scale_x_log10()scale_y_log10()
+plot1
+
+plot2 = ggplot(data=ws.factored.v2, aes(x=Fold.Change,y=primerset))+ geom_jitter()+scale_x_log10() +geom_jitter(aes(color=EGF, shape=Cell.Line))
+plot2
+
+plot3 = ggplot(data=subset(ws.factored.v2,ws.factored.v2$Cell.Line=='HMEC' & ws.factored.v2$Density=='High'), aes(x=Fold.Change,y=primerset))+ geom_jitter()+scale_x_log10() +geom_jitter(aes(color=EGF, shape=TGFB))
+plot3
+pairs()
